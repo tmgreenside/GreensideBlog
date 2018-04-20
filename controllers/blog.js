366d7f62d499154479fca1937e06f76cca40969a
tmgreenside <trevmg7@gmail.com>
2018-04-19 22:51:59 -0700

    Changed to a prepared statement in accounts.js exports.blogPost method

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,63 +1,68 @@
 var mysql = require('mysql');
 
 var dbConfig = {
-    server:'localhost',
-    database:'GreensideBlog',
-    user:'trevapp',
-    password:'bowers321',
-    port:3306,
+    server: 'localhost',
+    database: 'GreensideBlog',
+    user: 'trevapp',
+    password: 'bowers321',
+    port: 3306,
     multipleStatements: false // prevent certain types of SQL injection
 };
 
-const con = mysql.createConnection(dbConfig);
+var con = mysql.createConnection(dbConfig);
 
-exports.blogPost = function(req, res) {
+exports.blogPost = function (req, res) {
 
-	authenticator = "SELECT * FROM Accounts WHERE username = '" + req.body.username + "' AND password = '" + req.body.userpass + "'";
-	con.query(authenticator, function(err, result) {
-		if(err || result.length == 0) res.redirect('/error');
-		else {
-			testquery = "INSERT INTO Posts (postContent, postDate) VALUES ('" + req.body.blogger + "', NOW())";
-			con.query(testquery, function(err, result) {
-				res.redirect('/blog');
-			});
-		}
-	});
-}
+    var username = req.body.username;
+    var passenter = req.body.userpass;
+    // attempting to block SQL injection with prepared statement
+    authenticator = "SELECT * FROM Accounts WHERE username = ? AND password = ?";
+    con.query(authenticator, [username, passenter], function (err, result) {
+        if (err || result.length === 0)
+            res.redirect('/error');
+        else {
+            testquery = "INSERT INTO Posts (postContent, postDate) VALUES ('" + req.body.blogger + "', NOW())";
+            con.query(testquery, function (err, result) {
+                res.redirect('/blog');
+            });
+        }
+    });
+};
 
-exports.loadBlog = function(req, res) {
-	con.query("SELECT postContent, postDate FROM Posts", function(err, result) {
-		res.render('blog.html', {results: result});
-	});
-}
+exports.loadBlog = function (req, res) {
+    con.query("SELECT postContent, postDate FROM Posts", function (err, result) {
+        res.render('blog.html', {results: result});
+    });
+};
 
-exports.createAcct = function(req, res) {
-	res.render('createBlogAcct.html');
-}
+exports.createAcct = function (req, res) {
+    res.render('createBlogAcct.html');
+};
 
-exports.saveAcct = function(req, res) {
+exports.saveAcct = function (req, res) {
 
+    if (req.body.userpass !== req.body.userpass2) {
+        res.redirect('/error');
+        return;
+    } else if (req.body.username === "") {
+        res.redirect('/error');
+        return;
+    }
 
-	if (req.body.userpass != req.body.userpass2) {
-		res.redirect('/error');
-		return;
-	} else if (req.body.username == "") {
-		res.redirect('/error');
-		return;
-	}
-	
-	con.query("SELECT * FROM BloggerKeys WHERE acctKey = '" + req.body.acctKey + "'", function(err, result) {
-		if(err || result.length == 0) {
-			res.redirect('/error');
-			return;
-		}
-	});
+    con.query("SELECT * FROM BloggerKeys WHERE acctKey = '" + req.body.acctKey + "'", function (err, result) {
+        if (err || result.length === 0) {
+            res.redirect('/error');
+            return;
+        }
+    });
 
-	insertQuery = "INSERT INTO Accounts (username, password, acctKey) VALUES (";
-	insertQuery += "'" + req.body.username + "', '" + req.body.userpass + "', '" + req.body.acctKey + "')";
-	con.query(insertQuery, function(err, result) {
-		if(err) res.redirect('/error');
-		else res.redirect('/blog');
-	});
-	
-}
+    insertQuery = "INSERT INTO Accounts (username, password, acctKey) VALUES (";
+    insertQuery += "'" + req.body.username + "', '" + req.body.userpass + "', '" + req.body.acctKey + "')";
+    con.query(insertQuery, function (err, result) {
+        if (err)
+            res.redirect('/error');
+        else
+            res.redirect('/blog');
+    });
+
+};
